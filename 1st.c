c9a7e9961b911c971028ce02b3eafb713683ac5a
shihraw <93762778+shihraw@users.noreply.github.com>
2021-12-20 16:56:15 +0530

    Add files via upload

---
+++ 1st.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter 1st Number: \n");
+    scanf("%d", &a);
+    printf("Enter 2nd Number: \n");
+    scanf("%d", &b);
+    printf("3*a - 8*b is %d\n",3*a - 8*b);
+
+   
+} 
